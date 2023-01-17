645b94bc13a756403c98e1b00960565ce29179b3
Ahmed HOSNI <hosniah@users.noreply.github.com>
2023-01-17 20:45:19 +0100

    Update index.sh

--- nuke-layer/index.sh
+++ nuke-layer/index.sh
@@ -1,6 +1,6 @@
 function handler () {
     aws cloudformation create-stack --stack-name cfn-nuke-stack  \
     --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
-    --template-url https://cf-template-sandboxes.s3.amazonaws.com/aws-wipe-service.yaml  \
+    --template-url https://cf-template-datascientest-sandboxes.s3.amazonaws.com/aws-wipe-service.yaml  \
     --parameters ParameterKey=GitUser,ParameterValue='hosniah' ParameterKey=GitRepo,ParameterValue='aws-nuke-service' ParameterKey=GitBranch,ParameterValue=master ParameterKey=GitToken,ParameterValue='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' ParameterKey=AWSNukeVersionNumber,ParameterValue='2.12.0' ParameterKey=AWSNukeConfigFile,ParameterValue='aws-nuke-config/config.yaml' ParameterKey=AWSNukeProfileName,ParameterValue='nuke'
 }
