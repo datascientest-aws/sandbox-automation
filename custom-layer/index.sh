d3ab69057a8ac613989ed6febb89c74b44eef1dd
antho-data <83237498+antho-data@users.noreply.github.com>
2023-02-02 16:07:10 +0100

    Update index.sh

--- custom-layer/index.sh
+++ custom-layer/index.sh
@@ -31,5 +31,5 @@ function handler () {
     aws cloudformation create-stack --stack-name cfn-freeze-stack  \
     --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
     --template-url https://cf-template-datascientest-sandboxes.s3.amazonaws.com/aws-freeze-service.yaml  \
-    --parameters ParameterKey=NotificationEmailAddress,ParameterValue='ahmedhosni.contact@gmail.com' ParameterKey=WhenToExecute,ParameterValue='cron(0 0 * * ? *)' ParameterKey=RetentionInDays,ParameterValue=14 ParameterKey=AWSFreezeProfileName,ParameterValue=freeze
+    --parameters ParameterKey=NotificationEmailAddress,ParameterValue='dst-student@datascientest.com' ParameterKey=WhenToExecute,ParameterValue='cron(0 0 * * ? *)' ParameterKey=RetentionInDays,ParameterValue=14 ParameterKey=AWSFreezeProfileName,ParameterValue=freeze
 }
