05b3ed203e4c7ae9953249eaa4b627b8c0b83cc7
antho-data <jaillet.a74@gmail.com>
2023-02-02 15:05:37 +0000

    change script var

--- init.sh
+++ init.sh
@@ -15,7 +15,11 @@ fi
  
 read -r -p  "* Enter the name of the sandbox user:" username
 read -r -p  "* Enter the sandbox user  password:" userpass
-read -r -p  "* Enter the email of the sandbox user to notify (Student email):" email_student
+read -r -p  "* Enter the billing warning level (This will stop running resources) :" warning_level
+read -r -p  "* Enter the billing critical level (This triggers the aws nuke pipeline) :" critical_level
+read -r -p  "* Enter the email of the sandbox user to notify (Student email): " email_student
+read -r -p  "* Enter the datascientest admin email to notify :" email_datascientest
+
 
 # Creating first function to create a bucket 
 
@@ -26,7 +30,7 @@ function initsandbox()
   --stack-name sandbox-init-stack    --template-body file://aws-user-service.yaml \
   --parameters ParameterKey=UserName,ParameterValue=$username ParameterKey=Password,ParameterValue=$userpass ParameterKey=SESToEmail,ParameterValue=$email_student  --region eu-west-3
 
-  # Waite until the stack status is CREATE_COMPLETE
+  # Wait until the stack status is CREATE_COMPLETE
    
  #  until [ stack_status=$(aws cloudformation describe-stacks --stack-name sandbox-init-stack  --output json --query 'Stacks[0].StackStatus') = "CREATE_COMPLETE" ]
  #  do
@@ -56,10 +60,6 @@ function invokeConfigLambda()    {
  
 function setupAlerts()
 {
-   read -r -p  "* Enter the billing critical level (This triggers the aws nuke pipeline) :" critical_level
-   read -r -p  "* Enter the billing warning level (This will stop running resources) :" warning_level
-   read -r -p  "* Enter the email of the sandbox user to notify (Student email): " email_student
-   read -r -p  "* Enter the datascientest admin email to notify :" email_datascientest
 
    #Fetch the S3 bucket name, it has been createed randomly by the first stack:
    S3BucketName=$(aws s3api list-buckets --query 'Buckets[*].[Name]' --output text | grep "cfn-datascientest-sandbox-templates-repo" | head -n 1)
