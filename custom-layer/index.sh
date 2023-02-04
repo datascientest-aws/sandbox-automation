1524d48fce59b040038797f60cd590754dfe28e3
antho-data <83237498+antho-data@users.noreply.github.com>
2023-02-04 23:49:41 +0100

    Update index.sh

--- custom-layer/index.sh
+++ custom-layer/index.sh
@@ -1,16 +1,14 @@
 function handler () {
-    group_name='datascientest-readonlyusers'
     policy_arn='arn:aws:iam::aws:policy/ReadOnlyAccess'
     # Create the Readonly IAM group and assign the right managed policy
     # https://us-east-1.console.aws.amazon.com/iam/home?region=us-east-1#policies/arn:aws:iam::aws:policy/ReadOnlyAccess 
-    aws iam create-group --group-name $group_name
     aws iam attach-group-policy --group-name "$group_name" --policy-arn "$policy_arn" 
 
     # Publish an SNS message to the right topic
     topic=$(aws sns list-topics | jq -r ".Topics[ ] | .TopicArn" | grep BillingEmailAlertTopic )
     aws sns publish --topic-arn $topic --message 'You reached the warning level of your AWS sandbox budget. We’ll stop your was resources to reduce unwanted billing.'
 
-    # Make all users Read Only by adding them to the right group
+    # Make all users Read Only by adding them to the right Policy
     for user in $(aws iam list-users --query '[Users[].UserName]' --output text) ; do 
 
       # Remove all the policies directly attached to the user
@@ -23,8 +21,8 @@ function handler () {
         aws iam remove-user-from-group --user-name $user --group-name $iamgroup
       done
       
-      # Add the user to the newly created read only IAM group
-      aws iam add-user-to-group --user-name $user --group-name $group_name
+      # Add the user the readOnly Policy
+      aws iam attach-user-policy --user-name $user --policy-arn $policy_arn
     done
 
     #GITHUBToken should be added as an environment variable to this lambda.
